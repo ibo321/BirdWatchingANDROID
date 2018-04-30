daddaf0d8e6c6d942517e6ece6271cc33d4c1844
ibo321 <iboo96_96@hotmail.com>
2018-04-30 23:50:44 +0200

    lidt her og der

--- app/src/main/java/com/example/bruger/birdwatching/ObservationsActivities/AddObservation.java
+++ app/src/main/java/com/example/bruger/birdwatching/ObservationsActivities/AddObservation.java
@@ -3,10 +3,13 @@ package com.example.bruger.birdwatching.ObservationsActivities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.widget.EditText;
 
 import com.example.bruger.birdwatching.R;
 
 public class AddObservation extends AppCompatActivity {
+
+    EditText userId, englishName, danishName, population, latitude, longitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,8 +17,20 @@ public class AddObservation extends AppCompatActivity {
         setContentView(R.layout.activity_add_observation);
 
         Toolbar toolbar = findViewById(R.id.app_bar);
-        toolbar.setTitle("Add an observation");
+//        toolbar.setTitle("");
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        findViewsById();
+    }
+
+    public void findViewsById(){
+        userId = findViewById(R.id.addUserId);
+        englishName = findViewById(R.id.addEnglishName);
+        danishName = findViewById(R.id.addDanishName);
+        population = findViewById(R.id.addPopulation);
+        latitude = findViewById(R.id.addLatitude);
+        longitude = findViewById(R.id.addLongitude);
+
     }
 }
